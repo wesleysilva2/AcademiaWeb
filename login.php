b81aa05a4c36cde4ca2e536a44dbda263a0e92fb
wesleysilva2 <ede8021@gmail.com>
2022-06-08 00:19:41 -0300

    Adicionado todas as paginas de descrição para cada curso, login ainda não esta funcional, focar em outros pontos e deixar por ultimo, proximo passos é na pagina de academias e pagina de pagamento

--- login.php
+++ login.php
@@ -1,26 +1,35 @@
 <?php
 
+session_start();
+$email_login = "";
+$senha_login = "";
+$errors = array(); 
+
 include_once("conexao.php");
 
-$email_login = $_POST['email_login'];
-$senha_login = MD5($_POST['senha_login']);
-$logar = $_POST['logar'];
-$conexao = new mysqli('localhost','root','','movimentofit');
+if (isset($_POST['logar'])) {
+    $email_login = mysqli_real_escape_string($conexao, $_POST['email_login']);
+    $senha_login = mysqli_real_escape_string($conexao, $_POST['senha_login']);
 
-if(isset($logar)){
-    $verifica = $conexao -> query("SELECT * FROM usuarios WHERE email = '$email_login' AND senha = '$senha_login'")
-    or die ("erro ao selecionar coluna");
-
-    $rows = $verifica -> num_rows;
-    if($rows <=0){
-        echo "Falha ao logar! E-mail ou senha incorretos";
-          
-
-    }else{
-        echo "Login Feito Com sucesso!";
-        header("location:paginaUser.html");
+    if (empty($email_login)) {
+        array_push($errors, "Email é requerido");
     }
+    if (empty($senha_login)) {
+        array_push($errors, "Senha é necessaria");
+    }
+
+    if (count($errors) == 0) {
+        $senha_login = md5($senha_login);
+        $query = "SELECT * FROM usuarios WHERE email='$email_login' AND senha='$senha_login'";
+        $results = mysqli_query($conexao, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['email'] = $email_login;
+          $_SESSION['success'] = "Você conseguiu logar";
+          header('location: paginaUser.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+
 }
-
-
 ?>
